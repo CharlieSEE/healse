98d2c0e72b97921ef5dfc04a64ef097a77382d43
Visu4lny <visu4lny@gmail.com>
2021-11-25 22:02:08 +0100

    add: sign up page

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import "../css/SignUpPage.css";
 
 const SignUpPage = () => {
-    return (
-        <div>
-            from
+  return (
+    <div className="signUpPage">
+      <h1 className="title">
+        Create your Healse Account
+      </h1>
+      <form>
+        <input type="email" className="inputField" placeholder="Email" />
+        <input type="text" className="inputField" placeholder="Name" />
+        <input type="password" className="inputField" placeholder="Password" />
+        <input type="password" className="inputField confirmationField" placeholder="Confirm" />
+        <div className="confirmationInfo">
+          Use 8 or more characters with a mix of letters, numbers & symbols
         </div>
-    )
+        <button className="btn">Sign up</button>
+      </form>
+    </div>
+  )
 }
 
 export default SignUpPage
