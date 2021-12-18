1d49c0d2f70ee4b22a72fc1ef92fac0c7792f6b8
Visu4lny <visu4lny@gmail.com>
2021-12-18 20:20:11 +0100

    Feat: google authentication

--- src/components/LogInPage.jsx
+++ src/components/LogInPage.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useHistory } from "react-router-dom";
 import "../css/LogInPage.css";
 
-const LogInPage = () => {
+function LogInPage() {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
 
   const auth = getAuth();
   const history = useHistory();
+  const provider = new GoogleAuthProvider();
   const login = (e) => {
     e.preventDefault();
     signInWithEmailAndPassword(auth, userEmail, userPassword)
@@ -21,7 +22,18 @@ const LogInPage = () => {
         console.error(`${errorCode} ${errorMessage}`);
       });
   };
-
+  const googleLogin = (e) => {
+    e.preventDefault();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result);
+        history.push("/dashboard");
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(`${errorCode} ${errorMessage}`);
+      });
+  }
   return (
     <div className="logInPage">
       <h1 className="logInTitle">
@@ -40,19 +52,21 @@ const LogInPage = () => {
           className="logInInputField"
           onChange={(e) => setUserEmail(e.target.value)}
           value={userEmail}
-          placeholder="Email"
-        />
+          placeholder="Email" />
         <input
           type="password"
           className="logInInputField"
           onChange={(e) => setUserPassword(e.target.value)}
           value={userPassword}
-          placeholder="Password"
-        />
+          placeholder="Password" />
         <input type="submit" value="Log in" />
       </form>
+      <button className="login-provider-button" onClick={googleLogin}>
+        {/* <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon" /> */}
+        <span> Login with <b>Google</b></span>
+      </button>
     </div>
   );
-};
+}
 
 export default LogInPage;
