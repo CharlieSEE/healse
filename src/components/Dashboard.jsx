50486ea7199168569dc8933022aa76639a33b802
Visu4lny <visu4lny@gmail.com>
2021-12-20 19:54:44 +0100

    Feat: insert measurements menu
    - add new folder and icon
    - add measurements structure and styles

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
 import StatsList from "./StatsList/StatsList";
 import "../css/DashboardPage.css";
+import plusIcon from '../images/plus-icon.png'
 
 const Dashboard = () => {
   const auth = getAuth();
   const history = useHistory();
+  const [inputOpen, setInputOpen] = useState(false);
+  const handleToggle = () => {
+    setInputOpen(!inputOpen);
+  }
   const logOut = () => {
     signOut(auth).then(() => {
       history.push("/");
@@ -21,6 +26,60 @@ const Dashboard = () => {
         <StatsList />
         <button onClick={logOut}>Logout</button>
       </div>
+      <button className="plusIconWrapper" onClick={handleToggle}>
+        {inputOpen ? "Close" : "Open"}
+        <img src={plusIcon} alt="plus sign" />
+      </button>
+      <ul className={`inputList ${inputOpen ? "showInput" : ""}`}>
+        <li>
+          <span>Weight:</span>
+          <input
+            type="text"
+            className="measurementInputField"
+            placeholder="kg"
+          />
+        </li>
+        <li>
+          <span>Waist:</span>
+          <input
+            type="text"
+            className="measurementInputField"
+            placeholder="cm"
+          />
+        </li>
+        <li>
+          <span>Hip:</span>
+          <input
+            type="text"
+            className="measurementInputField"
+            placeholder="cm"
+          />
+        </li>
+        <li>
+          <span>Chest:</span>
+          <input
+            type="text"
+            className="measurementInputField"
+            placeholder="cm"
+          />
+        </li>
+        <li>
+          <span>Thigh:</span>
+          <input
+            type="text"
+            className="measurementInputField"
+            placeholder="cm"
+          />
+        </li>
+        <li>
+          <span>Biceps:</span>
+          <input
+            type="text"
+            className="measurementInputField"
+            placeholder="cm"
+          />
+        </li>
+      </ul>
     </div>
   );
 };
