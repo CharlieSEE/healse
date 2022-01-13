f2b0c12e851acd1f648efad258099cd797efd062
Kamil Madej <33061144+CharlieSEE@users.noreply.github.com>
2022-01-13 19:03:18 +0100

    Database handling (#9)
    * WIP: Fetching data with from current date
    
    * Feat: Adding of current day
    
    * Fix: BMI calc

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
+import { doc, setDoc, getDoc, getFirestore } from "firebase/firestore";
 import StatsList from "./StatsList/StatsList";
 import plusIcon from "../images/plus-icon.png";
 import DataInputPanel from "./DataInputPanel";
@@ -13,6 +14,15 @@ const Dashboard = () => {
   const auth = getAuth();
   const history = useHistory();
   const [inputOpen, setInputOpen] = useState(false);
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    const date = new Date();
+    const currentDate = `${date.getDate()}-${
+      date.getMonth() + 1
+    }-${date.getFullYear()}`;
+    setDate(currentDate);
+  }, []);
 
   const handleToggleInsertMenu = () => {
     setInputOpen(!inputOpen);
@@ -24,17 +34,37 @@ const Dashboard = () => {
   };
   const addData = async (data) => {
     handleToggleInsertMenu();
-    console.log(data);
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const db = getFirestore();
+    const factorOne = data.Weight * 1.082 + 94.42;
+    const factorTwo = data.Waist * 4.15;
+    const leanBodyMass = factorOne - factorTwo;
+    const bodyFatWeight = data.Weight - leanBodyMass;
+    let height = 0;
+    data.BodyFat = Math.floor((bodyFatWeight * 100) / data.Weight);
+    // Getting height
+    const docRef = doc(db, "users", user.uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      height = docSnap.data().Height;
+    }
+    data.BMI = (data.Weight / ((height * height) / 10000)).toFixed(2);
+    await setDoc(doc(db, "users", auth.currentUser.uid, "mes", date), data);
+    alert("Data added");
   };
 
   return (
     <div className="dashboardPage">
+      <SectionWrapper>
+        <h1>{date}</h1>
+      </SectionWrapper>
       {inputOpen ? <DataInputPanel onSave={addData} /> : null}
       <SectionWrapper>
         <Chart />
       </SectionWrapper>
       <SectionWrapper>
-        <StatsList />
+        <StatsList date={date} />
         <button onClick={logOut}>Logout</button>
       </SectionWrapper>
       {inputOpen ? null : (
--- src/components/StatsList/StatsList.jsx
+++ src/components/StatsList/StatsList.jsx
@@ -1,48 +1,89 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StatsListElement from "./StatsListElement";
+import { getAuth } from "firebase/auth";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
 
-const StatsList = () => {
+const StatsList = ({ date }) => {
+  const [data, setData] = useState({
+    Weight: 0,
+    BMI: 0,
+    BodyFat: 0,
+    Waist: 0,
+    Hip: 0,
+    Chest: 0,
+    Thigh: 0,
+    Biceps: 0,
+  });
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    const db = getFirestore();
+    const fetchData = async () => {
+      const date = new Date();
+      const currentDate = `${date.getDate()}-${
+        date.getMonth() + 1
+      }-${date.getFullYear()}`;
+      console.log(date);
+      const docRef = doc(db, "users", user.uid, "mes", currentDate);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setData(docSnap.data());
+      }
+    };
+    fetchData();
+  }, [date]);
   return (
-    <div>
-      <StatsListElement name="Weight" icon="icon" change="change" value={82} />
-      <StatsListElement name="BMI" icon="icon" change="change" value={26} />
+    <>
+      {/* <h3>{date}</h3> */}
+      <StatsListElement
+        name="Weight"
+        icon="icon"
+        change="change"
+        value={data.Weight}
+      />
+      <StatsListElement
+        name="BMI"
+        icon="icon"
+        change="change"
+        value={data.BMI}
+      />
       <StatsListElement
         name="Body Fat"
         icon="icon"
         change="change"
-        value={19}
+        value={data.BodyFat}
       />
       <StatsListElement
         name="Waist circumference"
         icon="icon"
         change="change"
-        value={96}
+        value={data.Waist}
       />
       <StatsListElement
         name="Hip circumference"
         icon="icon"
         change="change"
-        value={91}
+        value={data.Hip}
       />
       <StatsListElement
         name="Chest circumference"
         icon="icon"
         change="change"
-        value={110}
+        value={data.Chest}
       />
       <StatsListElement
         name="Thigh circumference"
         icon="icon"
         change="change"
-        value={94}
+        value={data.Thigh}
       />
       <StatsListElement
         name="Biceps circumference"
         icon="icon"
         change="change"
-        value={37}
+        value={data.Biceps}
       />
-    </div>
+    </>
   );
 };
 
