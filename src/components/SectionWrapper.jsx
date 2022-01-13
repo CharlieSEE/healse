c5215a3125c40791519560b37160119286ddb658
Kamil Madej <33061144+CharlieSEE@users.noreply.github.com>
2022-01-13 20:08:17 +0100

    Revert "Dashboard layout creation (#7)"
    This reverts commit 92ec4f560c0cb5d50cf2a2e68894620136814f8f.

--- src/components/SectionWrapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../css/SectionWrapper.module.css";
-
-const SectionWrapper = ({ children }) => {
-  return <div className={styles.sectionWrapper}>{children}</div>;
-};
-
-export default SectionWrapper;
