44e94d3a6c6d11d7cec9836a1c6f7aacd449dbcf
Visu4lny <visu4lny@gmail.com>
2022-06-11 15:21:06 +0200

    Feat: New signup page layout
    Both Mobile and Desktop layouts

--- src/components/Pages/SignUpPage/SignUpPage.jsx
+++ src/components/Pages/SignUpPage/SignUpPage.jsx
@@ -61,75 +61,77 @@ const SignUpPage = () => {
 
   return (
     <div className={styles.signUpPage}>
-      <h1 className={styles.signUpTitle}>
-        <div>Create your</div>
-        <div>
-          <span className={styles.blueText}> Healse </span>
-          Account
-        </div>
-      </h1>
-      <form className={styles.signUpForm} onSubmit={validateData}>
-        <input
-          type="email"
-          className={styles.signUpInputField}
-          onChange={(e) => setUserEmail(e.target.value)}
-          value={userEmail}
-          placeholder="Email"
-        />
-        <input
-          type="text"
-          className={styles.signUpInputField}
-          onChange={(e) => setUserName(e.target.value)}
-          value={userName}
-          placeholder="Name"
-        />
-        <input
-          type="password"
-          className={styles.signUpInputField}
-          onChange={(e) => setUserPassword(e.target.value)}
-          value={userPassword}
-          placeholder="Password"
-        />
-        <input
-          type="password"
-          className={styles.confirmationField}
-          onChange={(e) => setUserPasswordConfirmation(e.target.value)}
-          value={userPasswordConfirmation}
-          placeholder="Confirm"
-        />
-        <input
-          type="text"
-          className={styles.signUpInputField}
-          onChange={(e) => setUserHeight(e.target.value)}
-          value={userHeight}
-          placeholder="Height"
-        />
-        <input
-          type="number"
-          className={styles.signUpInputField}
-          onChange={(e) => setUserAge(e.target.value)}
-          value={userAge}
-          placeholder="Age"
-        />
-        <select
-          name="gender"
-          value={userGender}
-          onChange={(e) => setUserGender(e.target.value)}
-          required
-        >
-          <option value="">Chose your gender</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-        {wrongInput ? (
-          <div className={styles.confirmationInfo}>
-            Use 8 or more characters with a mix of letters, numbers & symbols
+      <div className={styles.signUpWrapper}>
+        <h1 className={styles.signUpTitle}>
+          <div>Create your</div>
+          <div>
+            <span className={styles.blueText}> Healse </span>
+            Account
           </div>
-        ) : (
-          <div className={styles.confirmationInfo}></div>
-        )}
-        <input type="submit" value="Sign in" />
-      </form>
+        </h1>
+        <form className={styles.signUpForm} onSubmit={validateData}>
+          <input
+            type="email"
+            className={styles.signUpInputField}
+            onChange={(e) => setUserEmail(e.target.value)}
+            value={userEmail}
+            placeholder="Email"
+          />
+          <input
+            type="text"
+            className={styles.signUpInputField}
+            onChange={(e) => setUserName(e.target.value)}
+            value={userName}
+            placeholder="Name"
+          />
+          <input
+            type="password"
+            className={styles.signUpInputField}
+            onChange={(e) => setUserPassword(e.target.value)}
+            value={userPassword}
+            placeholder="Password"
+          />
+          <input
+            type="password"
+            className={styles.confirmationField}
+            onChange={(e) => setUserPasswordConfirmation(e.target.value)}
+            value={userPasswordConfirmation}
+            placeholder="Confirm"
+          />
+          <input
+            type="text"
+            className={styles.signUpInputField}
+            onChange={(e) => setUserHeight(e.target.value)}
+            value={userHeight}
+            placeholder="Height"
+          />
+          <input
+            type="number"
+            className={styles.signUpInputField}
+            onChange={(e) => setUserAge(e.target.value)}
+            value={userAge}
+            placeholder="Age"
+          />
+          <select
+            name="gender"
+            value={userGender}
+            onChange={(e) => setUserGender(e.target.value)}
+            required
+          >
+            <option value="">Chose your gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          {wrongInput ? (
+            <div className={styles.confirmationInfo}>
+              Use 8 or more characters with a mix of letters, numbers & symbols
+            </div>
+          ) : (
+            <div className={styles.confirmationInfo}></div>
+          )}
+          <input type="submit" value="Sign in" />
+        </form>
+      </div>
     </div>
   );
 };
